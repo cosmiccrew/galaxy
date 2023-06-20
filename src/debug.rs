a269a5c8925f83a6740c37e76039846cf88d9bb0
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2023-06-20 18:33:21 +0100

    fix: fix debug mode
    * make GalaxyDebugPlugin work onto on debug_assertions cfg flag
    fix a doc comment
    * cargo-bundle asset path set only when conditions are correct for it

--- src/debug.rs
+++ src/debug.rs
@@ -1,10 +1,8 @@
-use bevy::input::common_conditions::input_toggle_active;
+use bevy::{input::common_conditions::input_toggle_active, window::close_on_esc};
 use bevy_inspector_egui::quick::{StateInspectorPlugin, WorldInspectorPlugin};
 
 use crate::prelude::*;
 
-/// Backslash = KeyCode::Backslash
-///
 /// Adds helpful features for debugging, like a [WorldInspectorPlugin] and
 /// [StateInspectorPlugin]. These can (by default) be toggled by pressing the
 /// Backslash (`\`)  keycode.
@@ -12,12 +10,16 @@ pub struct GalaxyDebugPlugin;
 
 impl Plugin for GalaxyDebugPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugin(
-            WorldInspectorPlugin::default().run_if(input_toggle_active(true, KeyCode::Backslash)),
-        )
-        .add_plugin(
-            StateInspectorPlugin::<EngineState>::default()
-                .run_if(input_toggle_active(true, KeyCode::Backslash)),
-        );
+        if cfg!(debug_assertions) {
+            app.add_plugin(
+                WorldInspectorPlugin::default()
+                    .run_if(input_toggle_active(true, KeyCode::Backslash)),
+            )
+            .add_plugin(
+                StateInspectorPlugin::<EngineState>::default()
+                    .run_if(input_toggle_active(true, KeyCode::Backslash)),
+            );
+            app.add_system(close_on_esc);
+        }
     }
 }
