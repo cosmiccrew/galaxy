c8ff709f6543693e9a4a7ee75210bfe544156b5c
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2023-08-29 16:57:32 +0200

    TDD: added main_menu_button_test
    * add the first proper TDD test, for main_menu_button_system (admittedly after making the function, but is a good POC)
    * renanme MainButton to MainMenuButton and covert it into an enum (replacing MainButtonType)

--- src/ui/main_menu.rs
+++ src/ui/main_menu.rs
@@ -6,7 +6,7 @@ impl Plugin for GalaxyMainMenuPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(WinitSettings::desktop_app())
             .add_systems(OnEnter(EngineState::MainMenu), setup)
-            .add_systems(Update, main_button_system)
+            .add_systems(Update, main_menu_button_system)
             .add_systems(OnExit(EngineState::MainMenu), teardown::<Loaded>);
     }
 }
@@ -27,21 +27,21 @@ enum ButtonState {
 }
 
 #[derive(Component, Debug, PartialEq, Eq, Clone, Copy)]
-enum MainButtonType {
+enum MainMenuButton {
     Local,
     Online,
     Settings,
 }
 
-#[derive(Component, Debug, Default, Reflect, PartialEq, Eq, Clone, Copy)]
-#[reflect(Component)]
-struct MainMenuButton;
+// #[derive(Component, Debug, Default, Reflect, PartialEq, Eq, Clone, Copy)]
+// #[reflect(Component)]
+// struct MainMenuButton;
 
-fn main_menu_button(parent: &mut ChildBuilder, button_type: MainButtonType, state: ButtonState) {
+fn main_menu_button(parent: &mut ChildBuilder, button_type: MainMenuButton, state: ButtonState) {
     let text = match button_type {
-        MainButtonType::Local => "Local (1-4)",
-        MainButtonType::Online => "Online (1-8)",
-        MainButtonType::Settings => "Settings",
+        MainMenuButton::Local => "Local (1-4)",
+        MainMenuButton::Online => "Online (1-8)",
+        MainMenuButton::Settings => "Settings",
     };
 
     parent
@@ -122,14 +122,14 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                     ..default()
                 })
                 .with_children(|parent| {
-                    main_menu_button(parent, MainButtonType::Local, ButtonState::Enabled);
-                    main_menu_button(parent, MainButtonType::Online, ButtonState::Disabled);
-                    main_menu_button(parent, MainButtonType::Settings, ButtonState::Disabled);
+                    main_menu_button(parent, MainMenuButton::Local, ButtonState::Enabled);
+                    main_menu_button(parent, MainMenuButton::Online, ButtonState::Disabled);
+                    main_menu_button(parent, MainMenuButton::Settings, ButtonState::Disabled);
                 });
         });
 }
 
-fn main_button_system(
+fn main_menu_button_system(
     mut engine_state: ResMut<NextState<EngineState>>,
     // mut game_state: ResMut<NextState<GameState>>,
     mut interaction_query: Query<
@@ -137,26 +137,29 @@ fn main_button_system(
             &Interaction,
             &mut BackgroundColor,
             &mut BorderColor,
-            &mut MainButtonType,
+            &mut MainMenuButton,
             &mut ButtonState,
         ),
         (Changed<Interaction>, With<Button>),
     >,
 ) {
+    println!("f");
     for (interaction, mut color, mut border_color, main_button_type, button_state) in
         &mut interaction_query
     {
+        println!("g");
         if *button_state == ButtonState::Enabled {
+            println!("e");
             match *interaction {
                 Interaction::Pressed => {
                     match *main_button_type {
-                        MainButtonType::Local => {
+                        MainMenuButton::Local => {
                             engine_state.set(EngineState::InGame);
                         }
-                        MainButtonType::Online => {
+                        MainMenuButton::Online => {
                             // engine_state.set(EngineState::InGame);
                         }
-                        MainButtonType::Settings => {
+                        MainMenuButton::Settings => {
                             // engine_state.set(EngineState::InGame);
                         }
                     }
@@ -190,10 +193,99 @@ fn main_button_system(
 #[cfg(test)]
 mod tests {
 
-    // use crate::{
-    //     prelude::*,
-    //     ui::main_menu::{main_button_system, main_menu_button, setup},
-    // };
+    use crate::prelude::*;
+
+    use super::{main_menu_button_system, ButtonState, MainMenuButton};
+
+    #[test]
+    fn test_main_menu_button_system() {
+        // Setup app
+        let mut app = App::new();
+
+        app.add_state::<EngineState>()
+            .add_systems(Update, main_menu_button_system);
+
+        let enabled = app
+            .world
+            .spawn((
+                Button,
+                ButtonState::Enabled,
+                MainMenuButton::Local,
+                Name::from("Enabled"),
+                Interaction::default(),
+                BackgroundColor::default(),
+                BorderColor::default(),
+            ))
+            .with_children(|parent| {
+                parent.spawn(TextBundle::default());
+            })
+            .id();
+
+        let disabled = app
+            .world
+            .spawn((
+                Button,
+                ButtonState::Disabled,
+                MainMenuButton::Local,
+                Name::from("Disabled"),
+                Interaction::default(),
+                BackgroundColor::default(),
+                BorderColor::default(),
+            ))
+            .with_children(|parent| {
+                parent.spawn(TextBundle::default());
+            })
+            .id();
+
+        app.update();
+
+        // nothing is clicked, so state should be the default
+        assert_eq!(
+            app.world
+                .get_resource::<State<EngineState>>()
+                .unwrap()
+                .get(),
+            &EngineState::default()
+        );
+
+        let mut disabled_interaction = app.world.get_mut::<Interaction>(disabled).unwrap();
+
+        *disabled_interaction = Interaction::Pressed;
+
+        //has to be performed twice, as state changes take two updates
+        app.update();
+        app.update();
+
+        //as this button is disabled, nothing should change
+        assert_eq!(
+            app.world
+                .get_resource::<State<EngineState>>()
+                .unwrap()
+                .get(),
+            &EngineState::default()
+        );
+
+        let mut enabled_interaction = app.world.get_mut::<Interaction>(enabled).unwrap();
+
+        println!("{:?}", &*enabled_interaction);
+
+        *enabled_interaction = Interaction::Pressed;
+
+        println!("{:?}", &*enabled_interaction);
+
+        //has to be performed twice, as state changes take two updates
+        app.update();
+        app.update();
+
+        //and here the state should have changed
+        assert_ne!(
+            app.world
+                .get_resource::<State<EngineState>>()
+                .unwrap()
+                .get(),
+            &EngineState::default()
+        );
+    }
 
     // #[test]
     // fn test_main_button_system() {
