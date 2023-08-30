19288852a3da122680700c0d79adcfe46add846d
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2023-08-30 12:39:42 +0200

    merge: Main menu UI (#16)
    * run cargo clippy --fix
    
    * Code refactoring and UI initialisation
    
    * made debug.rs only be imported when the feature flag is enabled
    * moved `add_loaded_component` into utils.rs from game.rs
    * change the default plugin setup (e.g. in the so-far unused polar.rs)
    * added a ui.rs with a `GalaxyUIPlugin`
    * added a ui folder and main_menu.rs file, with a `GalaxyMainMenuPlugin` within
    * added a parent node for `GalaxyMainMenuPlugin`
    * changed add_loaded_component to be in the `PreUpdate` stage rather than the `Update` stage
    
    * add aesprite files to .gitignore
    
    * add new item to bacon config
    
    * add a basic main menu ui setup
    
    * add the UI nodes for basic main menu buttons
    * prepare  future ui icons for use by adding them to the asset struct
    * changed default clear colour
    * fix log plugin cfg flag
    * remove "add_loaded_component" in favour of doing it manually, to prevent possible issues
    
    * added functionality to main menu buttons
    
    * TDD: added main_menu_button_test
    
    * add the first proper TDD test, for main_menu_button_system (admittedly after making the function, but is a good POC)
    * renanme MainButton to MainMenuButton and covert it into an enum (replacing MainButtonType)
    
    * added a suitable font + cleanup
    
    * cleaned `game.rs` + added the GameState to the plugin
    * renamed `state.rs` to `states.rs`
    * removed an ugly format from `ui.rs`
    * added `slkscre.ttf` font, used it in `main_menu.rs` for the buttons
    * inited a settings.rs file
    
    * replace the main_menu_button fn with a closure
    
    * trying TDD - main_menu_button_system update
    
    * it helps if your tests are correct...
    
    * satisfy the test (and noted hover oversight)
    
    * undo the newest change in the test
    
    * revert the change by following the test - TDD works!
    
    * add some `Name` components to some UI nodes

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,36 @@
-#![allow(unused)]
+// #![allow(unused)]
 #![allow(clippy::type_complexity, clippy::needless_update)]
-#![warn(dead_code)]
+// #![warn(dead_code)]
+#![allow(unused_variables)]
 
+#[cfg(feature = "debug")]
 pub mod debug;
 pub mod game;
 pub mod loading;
 pub mod player;
 pub mod polar;
 pub mod shaders;
-pub mod state;
+pub mod states;
+pub mod ui;
 pub mod utils;
 
 pub mod prelude {
 
     pub use std::f32::consts::*;
 
-    pub use bevy::{prelude::*, reflect::*};
+    pub use bevy::{prelude::*, reflect::*, winit::WinitSettings};
 
     pub use anyhow::{anyhow, bail, ensure, Result};
     pub use rand::prelude::*;
 
+    #[cfg(feature = "debug")]
     pub use crate::debug::*;
     pub use crate::game::*;
     pub use crate::loading::*;
     pub use crate::player::*;
     pub use crate::polar::*;
     pub use crate::shaders::{cloud_cover::*, consts::*, earthlike::*, *};
-    pub use crate::state::*;
+    pub use crate::states::*;
+    pub use crate::ui::*;
     pub use crate::utils::*;
 }
---
+++ src/ui.rs
@@ -0,0 +1,15 @@
+use crate::prelude::*;
+
+pub mod main_menu;
+
+use self::main_menu::GalaxyMainMenuPlugin;
+
+pub struct GalaxyUIPlugin;
+
+impl Plugin for GalaxyUIPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins(GalaxyMainMenuPlugin);
+        // app.add_systems(OnEnter(EngineState::InGame), setup)
+        //     .add_systems(OnExit(EngineState::InGame), teardown::<Loaded>);
+    }
+}
