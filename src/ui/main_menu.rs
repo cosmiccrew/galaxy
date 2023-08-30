2c7a6c1e9627f1850c7e7898e00fa26d13d3aaaf
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2023-08-30 12:11:17 +0200

    revert the change by following the test -TDD works

--- src/ui/main_menu.rs
+++ src/ui/main_menu.rs
@@ -142,7 +142,6 @@ fn main_menu_button_system(
         ),
         (Changed<Interaction>, With<Button>),
     >,
-    mut flag: Local<bool>,
 ) {
     for (interaction, mut color, mut border_color, main_button_type, button_state) in
         &mut interaction_query
@@ -150,31 +149,26 @@ fn main_menu_button_system(
         if *button_state == ButtonState::Enabled {
             match *interaction {
                 Interaction::Pressed => {
-                    if !*flag {
-                        *flag = true;
+                    match *main_button_type {
+                        MainMenuButton::Local => {
+                            //in future, this should go to another main menu section, where
+                            engine_state.set(EngineState::InGame);
+                        }
+                        MainMenuButton::Online => {
+                            // engine_state.set(EngineState::InGame);
+                        }
+                        MainMenuButton::Settings => {
+                            // engine_state.set(EngineState::InGame);
+                        }
                     }
                 }
-                interation => {
-                    if *flag {
-                        match *main_button_type {
-                            MainMenuButton::Local => {
-                                //in future, this should go to another main menu section, where
-                                engine_state.set(EngineState::InGame);
-                            }
-                            MainMenuButton::Online => {
-                                // engine_state.set(EngineState::InGame);
-                            }
-                            MainMenuButton::Settings => {
-                                // engine_state.set(EngineState::InGame);
-                            }
-                        }
-                    } else if interaction == &Interaction::Hovered {
-                        *color = HOVERED_BUTTON.into();
-                        border_color.0 = Color::DARK_GRAY;
-                    } else if interaction == &Interaction::None {
-                        *color = ENABLED_BUTTON.into();
-                        border_color.0 = Color::BLACK;
-                    }
+                Interaction::Hovered => {
+                    *color = HOVERED_BUTTON.into();
+                    border_color.0 = Color::DARK_GRAY;
+                }
+                Interaction::None => {
+                    *color = ENABLED_BUTTON.into();
+                    border_color.0 = Color::BLACK;
                 }
             }
         }
