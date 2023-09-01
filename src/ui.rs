fb838d73187faa174868e73e8a45d9a8b28f1577
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2023-09-01 17:28:51 +0200

    chore: random useless change

--- src/ui.rs
+++ src/ui.rs
@@ -9,7 +9,5 @@ pub struct GalaxyUIPlugin;
 impl Plugin for GalaxyUIPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(GalaxyMainMenuPlugin);
-        // app.add_systems(OnEnter(EngineState::InGame), setup)
-        //     .add_systems(OnExit(EngineState::InGame), teardown::<Loaded>);
     }
 }
